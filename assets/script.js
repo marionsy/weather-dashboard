40dfe36d19778da0cd47f6f780e3d5953f576f4e
marionsy <marionantoniette.sy@gmail.com>
2022-07-30 16:03:44 -0700

    added api calls in js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,90 @@
+var apiKey = "b6182655a0eb457214526577dea3c502";
+var searchButton = $("#search");
+
+// Current weather
+var currentWeather = {
+    cityName: null,
+    date: null,
+    icon: null,
+    temp: null,
+    humidity: null,
+    wind: null,
+    uv: null
+}
+
+function getLocation(city) {
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    fetch(requestUrl)
+    .then(function (response) {
+         return response.json();
+    })
+    .then(function(json) {
+        var lat = json[0].lat;
+        var lon = json[0].lon;
+        getCurrentWeather(lat, lon);
+        getFiveDay(lat, lon);
+        getUV(lat, lon); 
+    });
+}
+
+// city name > response.name
+// date > response.dt
+// icon for weather condition > response.weather.icon
+// temperarture = response.main.temp
+// humidity = response.main.humidity
+// wind speed = response.wind.speed
+function getCurrentWeather(lat, lon) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(json) {
+        currentWeather.cityName = json.name;
+        currentWeather.date = json.dt;
+        currentWeather.icon = json.weather[0].icon;
+        currentWeather.temp = json.main.temp;
+        currentWeather.humidity = json.main.humidity;
+        currentWeather.wind = json.wind.speed;
+        console.log(currentWeather);
+    });
+
+}
+
+// date > response.list[day].dt
+// temperature > response.list[day].main.temp
+// wind speed > response.list[day].wind.speed
+// humidity > response.list[day].main.humidity
+function getFiveDay(lat, lon) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
+
+    fetch(requestUrl)
+     .then(function (response) {
+       console.log(response);
+    })  
+}
+
+// uv index > response.value
+function getUV(lat, lon) {
+    var requestUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+
+    fetch(requestUrl)
+     .then(function (response) {
+       console.log(response);
+    })  
+}
+
+// currentWeather();
+
+$("#search").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $("#enterCity").val();
+    getLocation(city);
+})
+
 
 
 

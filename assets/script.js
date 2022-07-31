d3d3aac013e4c15b4d9b420757b5b1751513ccb2
marionsy <marionantoniette.sy@gmail.com>
2022-07-30 21:21:07 -0700

    added comments to files

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 var apiKey = "b6182655a0eb457214526577dea3c502";
 var searchButton = $("#search");
-
 var currWeather = $("#currentWeather");
 var weatherFore = $("#weatherForecast");
 
-var savedCities = JSON.parse(localStorage.getItem('cities'));
+var savedCities = JSON.parse(localStorage.getItem("cities"));
 
 if (savedCities) {
     getLocation(savedCities[savedCities.length - 1]);
 }
 
+// Function to get current weather
 function getLocation(city) {
     var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
 
@@ -30,12 +30,7 @@ function getLocation(city) {
         })
 }
 
-// city name > response.name
-// date > response.dt
-// icon for weather condition > response.weather.icon
-// temperature = response.main.temp
-// humidity = response.main.humidity
-// wind speed = response.wind.speed
+// Function to display current weather
 function getCurrentWeather(lat, lon) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
@@ -50,16 +45,16 @@ function getCurrentWeather(lat, lon) {
             var wind = getWind(json.wind.speed);
             var humidity = getHumidity(json.main.humidity);
 
-            currWeather.children('.title').text(title);
-            currWeather.children('.title').append(icon);
-            currWeather.children('.temp').text(temp);
-            currWeather.children('.wind').text(wind);
-            currWeather.children('.humidity').text(humidity);
+            currWeather.children(".title").text(title);
+            currWeather.children(".title").append(icon);
+            currWeather.children(".temp").text(temp);
+            currWeather.children(".wind").text(wind);
+            currWeather.children(".humidity").text(humidity);
         });
 
 }
 
-// uv index > response.value
+// Function to get UV index
 function getUV(lat, lon) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
 
@@ -72,26 +67,22 @@ function getUV(lat, lon) {
             var uvSpan = $("<span>");
 
             if (uvValue < 3) {
-                uvSpan.attr('class', 'badge badge-success');
+                uvSpan.attr("class", "badge badge-success");
             } else if (uvValue >= 3 && uvValue < 8) {
-                uvSpan.attr('class', 'badge badge-warning');
+                uvSpan.attr("class", "badge badge-warning");
             } else {
-                uvSpan.attr('class', 'badge badge-danger');
+                uvSpan.attr("class", "badge badge-danger");
             }
             uvSpan.text(uvValue);
 
-            currWeather.children('.uv').text("UV Index: ");
-            currWeather.children('.uv').append(uvSpan);
+            currWeather.children(".uv").text("UV Index: ");
+            currWeather.children(".uv").append(uvSpan);
 
             $(".currentWeatherCard").removeClass("d-none");
         });
 }
 
-// date > response.list[day].dt
-// temperature > response.list[day].main.temp
-// wind speed > response.list[day].wind.speed
-// humidity > response.list[day].main.humidity
-// icon > response.list[day].weather.icon
+// Function to get 5 day forecast
 function getFiveDay(lat, lon) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey + "&units=imperial";
 
@@ -108,13 +99,13 @@ function getFiveDay(lat, lon) {
                 var wind = getWind(json.list[i].wind.speed);
                 var humidity = getHumidity(json.list[i].main.humidity);
 
-                var forecastDay = $('#forecast-' + j);
-                forecastDay.children('.date').text(date);
-                forecastDay.children('.icon').empty();
-                forecastDay.children('.icon').append(icon);
-                forecastDay.children('.temp').text(temp);
-                forecastDay.children('.wind').text(wind);
-                forecastDay.children('.humidity').text(humidity);
+                var forecastDay = $("#forecast-" + j);
+                forecastDay.children(".date").text(date);
+                forecastDay.children(".icon").empty();
+                forecastDay.children(".icon").append(icon);
+                forecastDay.children(".temp").text(temp);
+                forecastDay.children(".wind").text(wind);
+                forecastDay.children(".humidity").text(humidity);
                 j++;
             }
 
@@ -122,17 +113,7 @@ function getFiveDay(lat, lon) {
         });
 }
 
-
-$("#search").on("click", function (event) {
-    event.preventDefault();
-
-    var city = $('#enterCity').val();
-
-    getLocation(city);
-
-    saveCity(city);
-})
-
+// Function to convert timestamp into todays date
 function getDate(timestamp) {
     var date = new Date(timestamp * 1000);
 
@@ -143,15 +124,17 @@ function getDate(timestamp) {
     return month + "/" + day + "/" + year;
 }
 
+// Function to get weather icon
 function getIcon(iconValue, iconDescription) {
     var iconEl = $("<img>");
     var iconSrc = "https://openweathermap.org/img/w/" + iconValue + ".png";
-    iconEl.attr('src', iconSrc);
-    iconEl.attr('alt', iconDescription)
+    iconEl.attr("src", iconSrc);
+    iconEl.attr("alt", iconDescription)
 
     return iconEl;
 }
 
+// Functions to get temp, wind and humidity
 function getTemp(tempValue) {
     return "Temp: " + tempValue + "°F";
 }
@@ -164,8 +147,9 @@ function getHumidity(humidityValue) {
     return "Humidity: " + humidityValue + " %";
 }
 
+// Function to save city to local storage and retrieve from storage
 function saveCity(city) {
-    var savedCities = JSON.parse(localStorage.getItem('cities'));
+    var savedCities = JSON.parse(localStorage.getItem("cities"));
 
     if (!savedCities) {
         savedCities = [];
@@ -173,10 +157,10 @@ function saveCity(city) {
 
     savedCities.push(city);
 
-    localStorage.setItem('cities', JSON.stringify(savedCities));
+    localStorage.setItem("cities", JSON.stringify(savedCities));
 
     var citybutton = $("<button>");
-    citybutton.attr('class', 'btn btn-secondary btn-lg btn-block');
+    citybutton.attr("class", "btn btn-secondary btn-lg btn-block");
     citybutton.css("background-color", "pink");
     citybutton.text(city);
 
@@ -185,5 +169,16 @@ function saveCity(city) {
         getLocation(city);
     })
 
-    $('#searchHistory').append(citybutton);
+    $("#searchHistory").append(citybutton);
 }
+
+// On click, call get weather functions
+$("#search").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $("#enterCity").val();
+
+    getLocation(city);
+
+    saveCity(city);
+})
